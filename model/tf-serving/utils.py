f5aadabb5c5a2b8782f0a299e7476690bf3920d7
surajkarki66 <suraj.karki500@protonmail.com>
2023-06-14 18:51:12 +0545

    updated utils.py

--- model/tf-serving/utils.py
+++ model/tf-serving/utils.py
@@ -2,9 +2,6 @@ import requests
 import json
 import numpy as np
 from tensorflow.keras.preprocessing import image
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
 
 def predict_top_classes(image_path):
     url = 'http://localhost:8601/v1/models/vgg_model:predict'
@@ -22,7 +19,6 @@ def predict_top_classes(image_path):
     predicted_class_indices = np.argsort(predictions[0])[::-1][:3]
 
     class_indices = {
-<<<<<<< HEAD
     0: 'Amaranthus Viridis',
     1: 'Basella Alba',
     2: 'Piper Betle',
@@ -53,38 +49,6 @@ def predict_top_classes(image_path):
     27: 'Ficus Auriculata',
     28: 'Santalum Album',
     29: 'Ocimum Tenuiflorum'
-=======
-    0: 'Arive-Dantu - Scientific name: Amaranthus',
-    1: 'Basale - Scientific name: Basella alba',
-    2: 'Betel - Scientific name: Piper betle',
-    3: 'Crape_Jasmine - Scientific name: Tabernaemontana divaricata',
-    4: 'Curry - Scientific name: Murraya koenigii',
-    5: 'Drumstick - Scientific name: Moringa oleifera',
-    6: 'Fenugreek - Scientific name: Trigonella foenum-graecum',
-    7: 'Guava - Scientific name: Psidium guajava',
-    8: 'Hibiscus - Scientific name: Hibiscus rosa-sinensis',
-    9: 'Indian_Beech - Scientific name: Pongamia pinnata',
-    10: 'Indian_Mustard - Scientific name: Brassica juncea',
-    11: 'Jackfruit - Scientific name: Artocarpus heterophyllus',
-    12: 'Jamaica_Cherry-Gasagase - Scientific name: Syzygium cumini',
-    13: 'Jamun - Scientific name: Syzygium cumini',
-    14: 'Jasmine - Scientific name: Jasminum spp.',
-    15: 'Karanda - Scientific name: Carissa carandas',
-    16: 'Lemon - Scientific name: Citrus limon',
-    17: 'Mango - Scientific name: Mangifera indica',
-    18: 'Mexican_Mint - Scientific name: Plectranthus amboinicus',
-    19: 'Mint - Scientific name: Mentha spp.',
-    20: 'Neem - Scientific name: Azadirachta indica',
-    21: 'Oleander - Scientific name: Nerium oleander',
-    22: 'Parijata - Scientific name: Nyctanthes arbor-tristis',
-    23: 'Peepal - Scientific name: Ficus religiosa',
-    24: 'Pomegranate - Scientific name: Punica granatum',
-    25: 'Rasna - Scientific name: Pluchea lanceolata',
-    26: 'Rose_apple - Scientific name: Syzygium jambos',
-    27: 'Roxburgh_fig - Scientific name: Ficus auriculata',
-    28: 'Sandalwood - Scientific name: Santalum album',
-    29: 'Tulsi - Scientific name: Ocimum tenuiflorum'
->>>>>>> bde84ff2f38da0f96395dcfb2305bd6b8fd8b900
 }
  
     predictions = predictions[0]
@@ -93,26 +57,6 @@ def predict_top_classes(image_path):
         for i in predicted_class_indices
     ]
 
-<<<<<<< HEAD
     prediction_response =  predicted_classes_with_probs
 
     return prediction_response
-=======
-    predicted_labels = [class_indices[i] for i in predicted_class_indices]
-
-    return predicted_labels
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image found in the request'})
-
-    image_file = request.files['image']
-    image_file.save('input_image.jpg')
-
-    predicted_classes = predict_top_classes('input_image.jpg')
-    return jsonify({'predictions': predicted_classes})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> bde84ff2f38da0f96395dcfb2305bd6b8fd8b900
